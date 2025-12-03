c4fc35ef3e6cf91297648596ef294bd4cbfb3a7e
Andrew Mugford <74738144+amugfordmugford@users.noreply.github.com>
2025-12-03 13:58:21 -0500

    Initial Commit

---
+++ ChoreLock/ChoreLockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChoreLockApp.swift
+//  ChoreLock
+//
+//  Created by Andrew Mugford on 2025-12-03.
+//
+
+import SwiftUI
+
+@main
+struct ChoreLockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
